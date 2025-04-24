ecdc112e6a960f16ee9a399bfe98131359ff241e
AdityaShuk1a <adityasarika0292004@gmail.com>
2025-04-24 13:20:48 +0530

    added name and registration number

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -7,6 +7,11 @@ function NavBar() {
         padding: "2vh"
     }} >
         Carbon Footprint Bot
+        <div className='text-xl absolute right-0 top-0'  >
+          <div>Name - Aditya Shukla Registration Number - 12312951</div>
+          <div>Name - Laukik Deshmukh Registration Number - 12311170</div>
+          <div>Name - Medha Jha Registration Number - 12305679</div>
+        </div>
     </div>
     </>
   )
